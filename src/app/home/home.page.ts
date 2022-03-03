86da1d1cbd962a20f9810fbe9a48ee45ee750aa6
Breiknay <kovalovevgeny00@gmail.com>
2022-03-03 20:54:44 +0300

    первые наброски ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {OnInit, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { ApiService } from '../api.service';
 
+import { IonInfiniteScroll } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit  {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  Items: any;
 
-  constructor() {}
+
+
+  constructor(private apiService: ApiService) {
+
+  }
+ngOnInit() {
+  this.apiService.getImage().subscribe((resp: any) => {
+    this.Items = resp;
+    console.log(resp);
+
+  });
+}
 
 }
